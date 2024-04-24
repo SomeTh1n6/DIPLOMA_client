077bf31378da006673c090ba2a0f5cc0146d87df
SomeTh1n6 <fregatmecht@mail.ru>
2024-04-24 17:52:11 +0300

    Добавил чат в проект, убрал аккаунт

--- src/main/java/ru/itmo/navigator_for_parents_app/ui/account/AccountFragment.kt
+++ src/main/java/ru/itmo/navigator_for_parents_app/ui/account/AccountFragment.kt
@@ -1,5 +1,6 @@
 package ru.itmo.navigator_for_parents_app.ui.account
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import ru.itmo.navigator_for_parents_app.ChatActivity
+import ru.itmo.navigator_for_parents_app.ChooseAgeActivity
 import ru.itmo.navigator_for_parents_app.data_files.InfoReg
 import ru.itmo.navigator_for_parents_app.data_files.Role
 import ru.itmo.navigator_for_parents_app.databinding.FragmentAccountBinding
@@ -19,10 +22,6 @@ class AccountFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-    private lateinit var editTextName : EditText
-    private lateinit var editTextCity : EditText
-    private lateinit var editTextNumber : EditText
-    private lateinit var editTextMail : EditText
     private lateinit var chageButton : Button
 
     override fun onCreateView(
@@ -35,45 +34,10 @@ class AccountFragment : Fragment() {
         val root: View = binding.root
 
         chageButton = binding.changeButton
-        editTextName = binding.enterNameText
-        editTextCity = binding.enterCityText
-        editTextNumber = binding.enterNumberText
-        editTextMail = binding.enterMailText
-
-        editTextName.isEnabled = false
-        editTextCity.isEnabled = false
-        editTextNumber.isEnabled = false
-        editTextMail.isEnabled = false
-
-        if (InfoReg.getRole() == Role.GUEST){
-            editTextMail.setText("-")
-        }
-        else{
-            editTextMail.setText(InfoReg.getEmail())
-        }
-
 
         chageButton.setOnClickListener {
-            if (InfoReg.getRole() == Role.GUEST){
-                Toast.makeText(activity,"Сначала надо зарегистрироваться",Toast.LENGTH_SHORT).show();
-            }
-            else{
-                if (chageButton.text == "Изменить") {
-                    chageButton.text = "Применить"
-                    editTextName.isEnabled = true
-                    editTextCity.isEnabled = true
-                    editTextNumber.isEnabled = true
-                    editTextMail.isEnabled = true
-                } else {
-                    chageButton.text = "Изменить"
-                    editTextName.isEnabled = false
-                    editTextCity.isEnabled = false
-                    editTextNumber.isEnabled = false
-                    editTextMail.isEnabled = false
-                    Toast.makeText(activity,"Изменения успешно применены",Toast.LENGTH_SHORT).show();
-                }
-            }
-
+            val intent = Intent(activity, ChatActivity::class.java)
+            startActivity(intent)
         }
 
         return root
